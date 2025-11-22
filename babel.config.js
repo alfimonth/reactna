e3c0f653ddd02fb352d484b97f6b408da56c5a8d
alfimonth <alvy1610@gmail.com>
2025-11-22 17:47:52 +0700

    chore: adding assets

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
   plugins: [
-    'react-native-worklets/plugin'
-  ]
+    'react-native-worklets/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+  ],
 };
--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,24 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = mergeConfig(getDefaultConfig(__dirname),{
-  /* your config */
+const defaultConfig = getDefaultConfig(__dirname);
+
+const {
+  resolver: { sourceExts, assetExts },
+} = defaultConfig;
+
+const config = mergeConfig(defaultConfig, {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
 
-module.exports = withNativeWind(config,  { input: "./global.css" })
+module.exports = withNativeWind(config, { input: './global.css' });
