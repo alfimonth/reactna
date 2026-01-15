c0b58a16059ed60a3bf0f872f455221288c8042b
alfimonth <alvy1610@gmail.com>
2026-01-15 18:14:45 +0700

    feat(component): add toogle on off animation

--- src/components/ui/ToggleOnOff.tsx
+++ src/components/ui/ToggleOnOff.tsx
@@ -1,20 +1,55 @@
-import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Animated, Pressable, View } from 'react-native';
 
 const ToggleOnOff = () => {
   const [isOn, setIsOn] = useState(false);
+  const toggleAnim = useRef(new Animated.Value(0)).current;
+  const slideDistance = -23;
+
+  useEffect(() => {
+    Animated.timing(toggleAnim, {
+      toValue: isOn ? 1 : 0,
+      duration: 220,
+      useNativeDriver: true,
+    }).start();
+  }, [isOn, toggleAnim]);
+
+  const translateX = toggleAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, slideDistance],
+  });
+
+  const rotate = toggleAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '-180deg'],
+  });
+  const textTranslateX = toggleAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 20],
+  });
 
   return (
-    <View className="bg-white rounded-full gap-[2px] border-main-3 border-[3px] flex-row items-center py-[.5] pr-[.9] pl-[2px]">
-      <Text className="text-main-3 font-bold">{isOn ? 'ON' : 'OFF'}</Text>
-      <View className="bg-white rounded-full border-main-3 border-[2.3px] p-[.9] relative">
-        <View className="rounded-full bg-main-3  p-2" />
+    <Pressable
+      onPress={() => setIsOn((prev) => !prev)}
+      className={`bg-white rounded-full gap-[2px] ${isOn ? 'border-red-500' : 'border-main-3'} border-[3px] flex-row items-center py-[.5] pr-[.9] pl-[2px]`}
+    >
+      <Animated.Text
+        className={`font-bold ${isOn ? 'text-red-500' : 'text-main-3'}`}
+        style={{ transform: [{ translateX: textTranslateX }] }}
+      >
+        {isOn ? 'ON' : 'OFF'}
+      </Animated.Text>
+      <Animated.View
+        className={`bg-white rounded-full ${isOn ? 'border-red-500' : 'border-main-3'} border-[2.3px] p-[.9] relative`}
+        style={{ transform: [{ translateX }, { rotate }] }}
+      >
+        <View className={`rounded-full ${isOn ? 'bg-red-500' : 'bg-main-3'} p-2`} />
         <View className="absolute -top-1 right-0 gap-1 -rotate-45">
           <View className="w-[3px] h-[2.5px] bg-white" />
           <View className="w-[3px] h-[2.5px] bg-white" />
         </View>
-      </View>
-    </View>
+      </Animated.View>
+    </Pressable>
   );
 };
 
